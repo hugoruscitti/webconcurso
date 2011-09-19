760845e2e414c5dc3641b51269f356ec564e4313
Hugo Ruscitti <hugoruscitti@gmail.com>
2011-09-19 12:32:50 -0300

    agregando.

--- initdb.py
+++ initdb.py
@@ -4,8 +4,8 @@ from app.db import db_engine, db_session
 
 try:
     ProjectTable.__table__.create(bind=db_engine)
-    db_session.add(ProjectTable('Hello World!'))
-    db_session.add(ProjectTable(u'こんにちは、世界！'))
+    db_session.add(ProjectTable('Hola mundo!!!'))
+    db_session.add(ProjectTable(u'hola!'))
     db_session.commit()
 except:
     pass
